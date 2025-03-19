c8e95ad9d0c993ba6e667610c3a90380f7706625
aqzsha <hepokz008@gmail.com>
2025-03-19 23:13:19 +0500

    practice 4: todoApp

---
+++ todoApp/frontend/src/router/index.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import ItemList from '../components/ItemList.vue';
+
+const routes = [{ path: '/', component: ItemList }];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
